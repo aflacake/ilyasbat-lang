25d1d8fc43b73ec5a92b134ef38db038a70e05ba
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-08-31 06:55:53 +0700

    `helpers\`
    `helpers\kalku.py`

--- modules/kalku.bat
+++ modules/kalku.bat
@@ -1,4 +1,15 @@
-if "%1"=="kalku" (
-    set /a result=%2%3%4
-    echo Hasil: %result%
+@echo off
+setlocal enabledelayedexpansion
+
+set "line=%*"
+for /f "tokens=1,2,3,4,5 delims= " %%a in ("%line%") do (
+    set "var=%%a"
+    set "equal=%%b"
+    set "val1=%%c"
+    set "op=%%d"
+    set "val2=%%e"
+)
+
+for /f "delims=" %%r in ('python helpers\kalku.py !val1! !op! !val2!') do (
+    endlocal & set "%var%=%%r"
 )
