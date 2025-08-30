99128c5da8ccffa6a753a70ee0dd023f1e9a3a2b
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-08-31 06:42:31 +0700

    Perubahan

--- main.bat
+++ main.bat
@@ -4,27 +4,18 @@ setlocal enabledelayedexpansion
 set "source_file=program.ibat"
 set "module_dir=modules"
 
-if not exist "!source_file!" (
-    echo File !source_file! tidak ditemukan.
-    exit /b
+for /f "usebackq delims=" %%A in ("%source_file%") do (
+    set "line=%%A"
+    for /f "tokens=1*" %%B in ("!line!") do (
+        set "cmd=%%B"
+        set "args=%%C"
+    )
+
+    if exist "%module_dir%\!cmd!.bat" (
+        call "%module_dir%\!cmd!.bat" !args!
+    ) else (
+        echo Perintah !cmd! tidak dikenal
+    )
 )
 
-for /f "usebackq delims=" %%A in ("!source_file!") do (
-    call :interpret "%%A"
-)
-
-goto :eof
-
-:interpret
-set "line=%~1"
-for /f "tokens=1*" %%C in (%line%) do (
-    set "cmd=%%C"
-    set "args=%%D"
-)
-
-if exist "%module_dir%\!cmd!.bat" (
-    call "%module_dir%\!cmd!.bat" !args!
-) else (
-    echo Perintah '!cmd!' tidak dikenali.
-)
-goto :eof
+endlocal
