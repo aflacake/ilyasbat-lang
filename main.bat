edce25c2012fd2103332665fb6917c284560ac3d
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-08-31 11:26:44 +0700

    Posisi dan penanganan `errorlevel`
    Menangkap keluaran dari `jika`, bisa menggunakan keluaran tersebut, dan menangini `errorlevel`.

--- main.bat
+++ main.bat
@@ -1,6 +1,6 @@
+@echo off
 REM main.bat
 
-@echo off
 setlocal enabledelayedexpansion
 
 set "source_file=program.ibat"
@@ -8,17 +8,30 @@ set "module_dir=modules"
 
 for /f "usebackq delims=" %%A in ("%source_file%") do (
     set "line=%%A"
+
     for /f "tokens=1*" %%B in ("!line!") do (
         set "cmd=%%B"
         set "args=%%C"
     )
 
     if exist "%module_dir%\!cmd!.bat" (
-        call "%module_dir%\!cmd!.bat" !args!
+        REM Jalankan modul dan tangkap output ke variabel
+        for /f "usebackq delims=" %%O in (`call "%module_dir%\!cmd!.bat" !args!`) do (
+            set "output=%%O"
+        )
+
+        REM Tangani errorlevel
         if errorlevel 1 (
-            echo Terjadi kesalahan saat menjalankan !cmd!, menghentikan eksekusi.
+            echo Terjadi kesalahan saat menjalankan !cmd!
             exit /b 1
         )
+
+        REM Tampilkan keluaran
+        if defined output (
+            echo !output!
+            set "output="
+        )
+
     ) else (
         echo Perintah !cmd! tidak dikenal
         exit /b 1
