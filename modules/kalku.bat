bc3b2de3cea97e015fd6f0dc39232afaf60b3112
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-08-31 16:38:38 +0700

    Ekspresi mentah
    Kirim ekspresi apa adanya ke Python.

--- modules/kalku.bat
+++ modules/kalku.bat
@@ -5,40 +5,13 @@ setlocal enabledelayedexpansion
 
 set "line=%*"
 
-set "line_no_space="
-for %%x in (!line!) do (
-    set "line_no_space=!line_no_space!%%x "
-)
-
-for /f "tokens=1,* delims==" %%a in ("!line_no_space!") do (
-    set "var=%%a"
-    set "expr=%%b"
-)
-
-if not defined expr (
-    echo Format salah. Gunakan: variabel = ekspresi
-    endlocal & exit /b 1
-)
-
-REM --------- Substitusi variabel di dalam ekspresi ----------
-set "evaluated_expr="
-for %%t in (!expr!) do (
-    set "token=%%t"
-    call set "value=%%%token%%%"
-    if defined value (
-        REM jika token adalah variabel, ganti dengan nilainya
-        set "evaluated_expr=!evaluated_expr!!value! "
-    ) else (
-        REM bukan variabel, biarkan tetap
-        set "evaluated_expr=!evaluated_expr!%%t "
-    )
-)
-
-REM ----------------------------------------------------------
-
 set "result="
-for /f "delims=" %%r in ('python helpers\kalku.py !evaluated_expr! 2^>nul') do (
+for /f "delims=" %%r in ('python helpers\kalku.py !line! 2^>nul') do (
     set "result=%%r"
+)
+
+for /f "tokens=1 delims==" %%a in ("!line!") do (
+    set "var=%%a"
 )
 
 if not defined result (
