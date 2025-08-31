a1b51ff30e173f0f5732edad528604cff935c03d
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-08-31 09:08:06 +0700

    Agar bisa kembalikan seperti mengembalikan pada umumnya
    Python menerima argumen (`%2`), Python mengeluarkan nilai (via `print`), dan Batch menangkapnya pakai `for /f` dan simpan ke variabel `%hasil%`.

--- modules/kembalikan.bat
+++ modules/kembalikan.bat
@@ -1,5 +1,13 @@
 REM modules/kembalikan.bat
 
+@echo off
+
 if "%1"=="kembalikan" (
-    set result=%2
+    setlocal enabledelayedexpansion
+
+    for /f "delims=" %%A in ('python helpers\kembalikan.py "%2"') do (
+        set "result=%%A"
+    )
+
+    endlocal & set "hasil=%result%"
 )
