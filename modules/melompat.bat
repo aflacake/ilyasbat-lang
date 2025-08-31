8a197e2b81abe873b581b227a6b51e39314e85e6
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-08-31 09:25:24 +0700

    Tidak bisa melompat karena `call`
    `goto` tidak bisa dilakukan karena adanya `call`, karena `goto` dan `call` hanya berlaku untuk skrip saat ini (melompat.bat) bukan (main.bat).

--- modules/melompat.bat
+++ modules/melompat.bat
@@ -1,5 +1,13 @@
 REM modules/melompat.bat
 
+@echo off
+
 if "%1"=="melompat" (
-    goto %2
+    setlocal enabledelayedexpansion
+
+    for /f "delims=" %%A in ('python helpers\melompat.py "%2"') do (
+        set "target=%%A"
+    )
+
+    endlocal & set "next_label=%target%"
 )
