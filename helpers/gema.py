5ee5a29e51d76cd67b35fc49af98e403ebc91cee
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-09-07 08:41:08 +0700

    Logika utama
    Semua logika (gabung _string_, cetak, validasi, dll.) ada di Python.

--- helpers/gema.py
+++ helpers/gema.py
@@ -2,6 +2,10 @@
 
 import sys
 
+def gema(teks: str):
+    """Cetak ulang teks ke layar (fungsi gema)."""
+    print(teks)
+
 if __name__ == "__main__":
     teks = " ".join(sys.argv[1:])
-    print(teks)
+    gema(teks)
