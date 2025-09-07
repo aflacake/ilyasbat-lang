9d1f83b44ad435bba6361cee77a1e78d25513aa7
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-09-07 08:32:46 +0700

    Logika utama
    Bukan _wrapper_.

--- helpers/tulis.py
+++ helpers/tulis.py
@@ -1,9 +1,13 @@
 # helpers/tulis.py
 
+import sys
+
 def tulis(args):
     if not args:
         print("[Tidak ada teks untuk ditulis]")
         return
-
     output = " ".join(args)
     print(output)
+
+if __name__ == "__main__":
+    tulis(sys.argv[1:])
