b8638d926eb4fa17945bf7f56ded00a29617f3c6
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-09-07 09:00:45 +0700

    Batch tidak main logika

--- modules/masukkan.bat
+++ modules/masukkan.bat
@@ -1,14 +1,5 @@
 @echo off
 REM modules/masukkan.bat
 
-if "%1"=="masukkan" (
-    setlocal enabledelayedexpansion
-
-    set "varname=%2"
-
-    for /f "delims=" %%A in ('python helpers\masukkan.py') do (
-        set "result=%%A"
-    )
-
-    endlocal & set "%varname%=%result%"
-)
+python helpers\masukkan.py %*
+exit /b %ERRORLEVEL%
