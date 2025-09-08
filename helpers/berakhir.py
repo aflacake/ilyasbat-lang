ec06fa8d803bae03b1b334c241209f4f7542922f
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-09-08 11:16:02 +0700

    Penghentian program
    Berisi penuh logika penghentian program (`sys.exit(0)`)

--- helpers/berakhir.py
+++ helpers/berakhir.py
@@ -1,5 +1,11 @@
 # helpers/berakhir.py
 
-def berakhir():
+import sys
+
+def main():
     print("[Program dihentikan melalui perintah 'berakhir']")
-    exit(0)
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
+
