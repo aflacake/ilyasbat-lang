b7e64320b2d275824737095496a8b731f29b39e8
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-09-08 11:02:48 +0700

    _Wrapper_
    Benar-benar kosong dari logika.

--- modules/menyimpan.bat
+++ modules/menyimpan.bat
@@ -1,13 +1,4 @@
 @echo off
 REM modules/menyimpan.bat
 
-if "%1"=="menyimpan" (
-    setlocal enabledelayedexpansion
-
-    set "key=%2"
-    set "value=%3"
-
-    python helpers\menyimpan.py "!key!" "!value!"
-
-    endlocal
-)
+python helpers\menyimpan.py %*
