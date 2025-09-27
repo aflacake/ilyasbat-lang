8e0b8150fe0ff8aee17af5926b57af764a4174d8
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-09-27 11:55:18 +0700

    Kurang konsisten dengan REPL
    Tidak konsisten dengan REPL, tidak ada _return_, dan tidak ada dukungan format lebih kompleks.
    
    Perbedaan penting:
    Bisa evaluasi ekspresi, bisa return string kalau `return_only=True`, dan masih kompatibel dipanggil dari CLI.

--- helpers/tulis.py
+++ helpers/tulis.py
@@ -1,26 +1,46 @@
 # helpers/tulis.py
 
 import sys
-import os
+from simpleeval import simple_eval
 
-def tulis(args, env=None):
+
+def tulis(args, env=None, return_only=False):
+    """
+    Print atau kembalikan hasil gabungan argumen.
+    - args: list of string token
+    - env: dict variabel
+    - return_only: kalau True, tidak print tapi return string
+    """
     if not args:
-        print("[Tidak ada teks untuk ditulis]")
-        return
+        msg = "[Tidak ada teks untuk ditulis]"
+        if return_only:
+            return msg
+        print(msg)
+        return msg
 
     hasil = []
     for arg in args:
         if (arg.startswith('"') and arg.endswith('"')) or (arg.startswith("'") and arg.endswith("'")):
             hasil.append(arg[1:-1])
-        elif env and arg in env:
-            hasil.append(str(env[arg]))
+        elif env:
+            try:
+                val = simple_eval(arg, names=env)
+                hasil.append(str(val))
+            except Exception:
+                if arg in env:
+                    hasil.append(str(env[arg]))
+                else:
+                    hasil.append(arg)
         else:
             hasil.append(arg)
 
     output = " ".join(hasil)
+
+    if return_only:
+        return output
     print(output)
+    return output
+
 
 if __name__ == "__main__":
-    import sys
     tulis(sys.argv[1:])
-
